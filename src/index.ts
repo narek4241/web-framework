ab164b943c88e956621a78bfb0a50ecc8477bdd8
narek4241 <narekghazaryan02@gmail.com>
2021-01-22 15:10:10 +0400

    models/User -> get,set

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('hello ts');
+import { User } from './models/User';
+
+const user = new User({ name: 'poghos' });
+
+user.set({ name: 'newName', age: 25 });
+
+console.log(user.get('name'), user.get('age'));
