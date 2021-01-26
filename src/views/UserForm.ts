38e8519d28c8b77c5260e87e1bb83c803c4dea25
narek4241 <narekghazaryan02@gmail.com>
2021-01-26 04:40:26 +0400

    UserForm -> event handling(eventsMap, bindEvents)

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,20 +1,50 @@
 export class UserForm {
   constructor(public parent: Element) {}
 
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick,
+      'mouseenter:h1': this.onHeaderHover,
+    };
+  }
+
+  onButtonClick(): void {
+    console.log('button clicked');
+  }
+
+  onHeaderHover(): void {
+    console.log('header hovered');
+  }
+
   template(): string {
     return `
     <div>
       <h1>Template Here</h1>
       <input />
+      <button>Click Me</button>
     </div>
   `;
   }
 
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (const eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+      const callback = eventsMap[eventKey];
+
+      fragment.querySelectorAll(selector).forEach((element: Element) => {
+        element.addEventListener(eventName, callback);
+      });
+    }
+  }
+
   render(): void {
-    const templateELement = document.createElement('template');
-    templateELement.innerHTML = this.template();
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content);
 
     // #task #res2 dom
-    this.parent.append(templateELement.content);
+    this.parent.append(templateElement.content);
   }
 }
