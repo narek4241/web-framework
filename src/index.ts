9c4519ed698e262a9ab5522a1881c6bfe6e3b2f0
narek4241 <narekghazaryan02@gmail.com>
2021-01-26 17:15:00 +0400

    UserForm -> Model properties, re-rendering on Model change

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import { User } from './models/User';
 import { UserForm } from './views/UserForm';
 
-const userForm = new UserForm(document.getElementById('root'));
+const user = User.buildUser({ name: 'USER', age: 30 });
+
+const userForm = new UserForm(document.getElementById('root'), user);
 
 userForm.render();
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,27 +1,44 @@
+import { User } from '../models/User';
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render();
+    });
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick,
-      'mouseenter:h1': this.onHeaderHover,
+      'click:.button-set-age': this.onSetAgeClick,
+      'click:.button-set-name': this.onSetNameClick,
     };
   }
 
-  onButtonClick(): void {
-    console.log('button clicked');
-  }
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  };
 
-  onHeaderHover(): void {
-    console.log('header hovered');
-  }
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+    const name = input.value;
+    this.model.set({ name });
+  };
 
   template(): string {
     return `
     <div>
       <h1>Template Here</h1>
-      <input />
-      <button>Click Me</button>
+      <p>User Name: ${this.model.get('name')}</p>
+      <p>User Age: ${this.model.get('age')}</p>
+      <div>
+          <input value=${this.model.get('name')} />
+          <button class="button-set-name">Update Name</button>
+          <button class="button-set-age">Set Random Age</button>
+      </div>
     </div>
   `;
   }
@@ -40,8 +57,11 @@ export class UserForm {
   }
 
   render(): void {
+    this.parent.innerHTML = '';
+
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+
     this.bindEvents(templateElement.content);
 
     // #task #res2 dom
